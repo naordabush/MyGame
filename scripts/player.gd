418cef953ab9ec2a5ec3bc4cc10541b579a9ab3b
OLD-INTERLEM\naor.dabush <naor.dabush@interlem.it>
2024-06-07 11:41:31 +0200

    updated files

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,10 +4,10 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes. default is set to 980
+# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var animated_sprite = $AnimatedSprite2D
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -18,19 +18,16 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction: -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
-	# Get the imput direction: -1, 0, 1 
 	
+	# Flip the Sprite
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
-
-# play animation
-
-
+	
+	# Play animations
 	if is_on_floor():
 		if direction == 0:
 			animated_sprite.play("idle")
@@ -38,11 +35,8 @@ func _physics_process(delta):
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
-
-
-
-
 	
+	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
--- scripts/slime.gd
+++ scripts/slime.gd
@@ -17,5 +17,4 @@ func _process(delta):
 		direction = 1
 		animated_sprite.flip_h = false
 	
-	
 	position.x += direction * SPEED * delta
